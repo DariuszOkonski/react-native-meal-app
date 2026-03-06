847ef9d177af61a4113f78efea81a53b6a180d76
Dariusz Okoński <darek200180@gmail.com>
2026-03-06 07:11:14 +0100

    add NavigationContainer to App

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <React.Fragment>
       <StatusBar style='light' />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </React.Fragment>
   );
 }
